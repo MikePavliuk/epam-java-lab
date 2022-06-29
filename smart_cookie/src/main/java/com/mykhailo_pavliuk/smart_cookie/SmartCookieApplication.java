e0c755f59fa836d845fda8bbbb5b161bf512bfa6
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2022-06-29 09:13:25 +0300

    Initialize empty Spring Web project with Spring Initializr

---
+++ smart_cookie/src/main/java/com/mykhailo_pavliuk/smart_cookie/SmartCookieApplication.java
@@ -0,0 +1,13 @@
+package com.mykhailo_pavliuk.smart_cookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCookieApplication.class, args);
+	}
+
+}
---
+++ smart_cookie/src/test/java/com/mykhailo_pavliuk/smart_cookie/SmartCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykhailo_pavliuk.smart_cookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
