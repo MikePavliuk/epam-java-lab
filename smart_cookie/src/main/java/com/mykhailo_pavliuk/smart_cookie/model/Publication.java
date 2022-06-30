c6ae6a234720a9fc0e694e8cbdb1558ddd3b456e
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2022-06-30 09:45:41 +0300

    Add relationship of Publication and PublicationInfo

--- smart_cookie/src/main/java/com/mykhailo_pavliuk/smart_cookie/model/Publication.java
+++ smart_cookie/src/main/java/com/mykhailo_pavliuk/smart_cookie/model/Publication.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @Builder
@@ -11,4 +12,5 @@ public class Publication {
 	private Long id;
     private Genre genre;
     private BigDecimal pricePerMonth;
+	private List<PublicationInfo> publicationInfos;
 }
